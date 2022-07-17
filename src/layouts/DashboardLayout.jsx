3bf77dcd6c534858223380dd19accdeddf1481c2
Tayyab Ferozi <ferozitayyab@gmail.com>
2022-07-17 23:36:12 +0500

    starts responsive of dashboard

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import Button from "../components/Button";
 
@@ -25,7 +25,13 @@ const tabsData = [
   },
 ];
 
-const DashboardLayout = () => {
+const DashboardLayout = ({ children }) => {
+  const [currTab, setCurrTab] = useState({
+    icon: "/assets/vectors/nav-tab-1.svg",
+    link: "/upcoming-interview",
+    title: "Upcoming Interview",
+  });
+
   return (
     <div className="dashboard-layout">
       <div className="layout-left">
@@ -46,10 +52,35 @@ const DashboardLayout = () => {
         </div>
       </div>
       <div className="layout-right">
-        <div className="head">
+        <div className="layout-head">
           <div className="head-left">
-            <div className="fs-22 mb-3">Hello, Jane</div>
+            <div className="mb-3 greeting">Hello, Jane</div>
             <h2>Welcome Back!</h2>
+
+            <div className="min-tabs">
+              <div className="layout-tabs">
+                <div className="active-item">
+                  <div className="left d-flex align-items-center gap-2">
+                    <img src={currTab.icon} alt={currTab.title} />
+                    <span className="fw-600">{currTab.title}</span>
+                  </div>
+                  <div className="right">
+                    <img src="/assets/vectors/chevron.svg" alt="chevron" />
+                  </div>
+                </div>
+                <div className="list">
+                  {tabsData.map((el, idx) => {
+                    const { icon, link, title } = el;
+
+                    return (
+                      <NavLink key={"tab-item" + idx} to={link}>
+                        <img src={icon} alt={title} /> {title}
+                      </NavLink>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
           </div>
           <div className="head-right">
             <div className="btns">
@@ -69,7 +100,7 @@ const DashboardLayout = () => {
           </div>
         </div>
 
-        <div className="main-layout-content"></div>
+        <div className="main-layout-content">{children}</div>
       </div>
     </div>
   );
--- src/screens/UpcomingInterview/index.jsx
+++ src/screens/UpcomingInterview/index.jsx
@@ -2,7 +2,122 @@ import React from "react";
 import DashboardLayout from "../../layouts/DashboardLayout";
 
 const UpcomingInterview = () => {
-  return <DashboardLayout>UpcomingInterview</DashboardLayout>;
+  return (
+    <DashboardLayout>
+      <div className="card-lg">
+        <h4 className="title">Upcoming Interviews</h4>
+
+        <div className="custom-table">
+          <div className="table-head">
+            <div className="row-item">
+              <div className="cell-item">Notebook</div>
+              <div className="cell-item">Candidate Name</div>
+              <div className="cell-item">Creator</div>
+              <div className="cell-item">Interview Date</div>
+              <div className="cell-item">Created</div>
+            </div>
+          </div>
+
+          <div className="table-body">
+            {[
+              {
+                name: "Jerome Bell",
+                creator: "Cameron Williamson",
+                date: "2021-12-31",
+                created: "An hour ago",
+              },
+              {
+                name: "Jerome Bell",
+                creator: "Cameron Williamson",
+                date: "2021-12-31",
+                created: "An hour ago",
+              },
+              {
+                name: "Jerome Bell",
+                creator: "Cameron Williamson",
+                date: "2021-12-31",
+                created: "An hour ago",
+              },
+              {
+                name: "Jerome Bell",
+                creator: "Cameron Williamson",
+                date: "2021-12-31",
+                created: "An hour ago",
+              },
+              {
+                name: "Jerome Bell",
+                creator: "Cameron Williamson",
+                date: "2021-12-31",
+                created: "An hour ago",
+              },
+              {
+                name: "Jerome Bell",
+                creator: "Cameron Williamson",
+                date: "2021-12-31",
+                created: "An hour ago",
+              },
+              {
+                name: "Jerome Bell",
+                creator: "Cameron Williamson",
+                date: "2021-12-31",
+                created: "An hour ago",
+              },
+              {
+                name: "Jerome Bell",
+                creator: "Cameron Williamson",
+                date: "2021-12-31",
+                created: "An hour ago",
+              },
+              {
+                name: "Jerome Bell",
+                creator: "Cameron Williamson",
+                date: "2021-12-31",
+                created: "An hour ago",
+              },
+              {
+                name: "Jerome Bell",
+                creator: "Cameron Williamson",
+                date: "2021-12-31",
+                created: "An hour ago",
+              },
+            ].map((el, idx) => {
+              const { name, creator, date, created } = el;
+
+              return (
+                <div key={"item-" + idx} className="row-item">
+                  <div className="cell-item">
+                    <div className="left">Notebook</div>
+                    <div className="right">
+                      <div className="d-flex align-items-center gap-2">
+                        <img src="/assets/vectors/play.svg" alt="play" />{" "}
+                        <span className="text-primary-1">Start Notebook</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="cell-item">
+                    <div className="left">Candidate Name </div>
+                    <div className="right">{name}</div>
+                  </div>
+                  <div className="cell-item">
+                    <div className="left">Creator</div>
+                    <div className="right">{creator}</div>
+                  </div>
+                  <div className="cell-item">
+                    <div className="left">Interview Date</div>
+                    <div className="right">{date}</div>
+                  </div>
+                  <div className="cell-item">
+                    <div className="left">Created</div>
+                    <div className="right">{created}</div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </DashboardLayout>
+  );
 };
 
 export default UpcomingInterview;
